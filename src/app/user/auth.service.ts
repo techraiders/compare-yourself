a5e604a3b202d2805cfbd816e8e8eb8117ace53e
Navneet Prakash <hello_navneet@hotmail.com>
2019-07-21 21:47:28 +0530

    Signs up, and confirms cognito  user.

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -6,11 +6,26 @@ import { Observable } from "rxjs/Observable";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { User } from "./user.model";
 
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser
+} from "amazon-cognito-identity-js";
+
+const POOL_DATA = {
+  UserPoolId: "ap-south-1_dgwscaMzO",
+  ClientId: "6b1u977f0ombmujvvqt5bvcp4s"
+};
+
+const userPool = new CognitoUserPool(POOL_DATA);
+
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser: CognitoUser;
+
   constructor(private router: Router) {}
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
@@ -19,17 +34,54 @@ export class AuthService {
       email: email,
       password: password
     };
+
+    const attrList: CognitoUserAttribute[] = [];
+
     const emailAttribute = {
       Name: "email",
       Value: user.email
     };
-    return;
+
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+
+    userPool.signUp(
+      user.username,
+      user.password,
+      attrList,
+      null,
+      (err, result) => {
+        if (err) {
+          this.authDidFail.next(true);
+          this.authIsLoading.next(false);
+          return;
+        } else {
+          this.authDidFail.next(false);
+          this.authIsLoading.next(false);
+          this.registeredUser = result.user;
+        }
+      }
+    );
+
+    //return;
   }
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
-      Username: username
+      Username: username,
+      Pool: userPool
     };
+
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code, true, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+      } else {
+        this.authDidFail.next(false);
+        this.authIsLoading.next(false);
+        this.router.navigate(["/"]);
+      }
+    });
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
